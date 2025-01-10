181ed923e2b2d73a2ddcea5e4efc361dcdfad357
cplee1 <c.p.lee@student.curtin.edu.au>
2025-01-10 10:28:23 +0800

    Tidied up plot and added P/DM for paper

--- src/psrutils/apps/plot_grid.py
+++ src/psrutils/apps/plot_grid.py
@@ -1,6 +1,7 @@
 import click
 import matplotlib.pyplot as plt
 import numpy as np
+import psrqpy
 
 import psrutils
 
@@ -19,25 +20,42 @@ def main(archives: tuple, bscr: int, ncols: int, nrows: int) -> None:
         tmp_data = psrutils.StokesCube.from_psrchive(archive, False, 1, 1, bscr)
         data_list.append(tmp_data)
 
+    plt.rcParams["mathtext.fontset"] = "dejavuserif"
+    plt.rcParams["text.usetex"] = True
+    plt.rcParams["font.family"] = "serif"
+    plt.rcParams["font.serif"] = "cm"
+    plt.rcParams["font.size"] = 12
+
     fig, axes = plt.subplots(
-        figsize=(ncols * 2, nrows * 2), ncols=ncols, nrows=nrows, dpi=300, tight_layout=True
+        figsize=(ncols * 2.1, nrows * 2), ncols=ncols, nrows=nrows, dpi=300, tight_layout=True
     )
+
+    query = psrqpy.QueryATNF()
+    print(f"Using PSRCAT v{query.get_version}")
+    psrs = query.get_pulsars()
 
     for row in range(nrows):
         for col in range(ncols):
             data = data_list[col + row * ncols]
             ax = axes[row, col]
-            data._archive.centre_max_bin()
+            snr = data.snr
+            if snr < 35:
+                data.bscrunch(64)
             prof = data.profile
             bins = np.arange(data.num_bin) / (data.num_bin - 1)
-            ax.plot(bins, prof, color="k")
+            ax.plot(bins, prof, color="k", linewidth=0.8)
             ax.set_yticklabels([])
             ax.set_xticks([0, 0.25, 0.5, 0.75, 1])
             ax.set_xticklabels(["0", "0.25", "0.5", "0.75", "1"])
             ax.set_xlim([bins[0], bins[-1]])
-            ax.set_title(f"{data._archive.get_source()}")
+            psrname = data._archive.get_source()
+            ax.set_title(
+                f"{psrs[psrname].Name}\nP={psrs[psrname].P0*1e3:.3f}  DM={psrs[psrname].DM:.2f}",
+                fontsize=12,
+            )
 
     fig.supxlabel("Pulse Phase [rot]")
-    fig.supylabel("Flux Density [arb]")
+    fig.supylabel("Flux Density [arb. units]")
     fig.savefig("grid.png")
+    fig.savefig("grid.pdf")
     plt.close()
