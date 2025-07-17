f6434d414492cb66e36dfdcec69a23f569119f77
cplee1 <c.p.lee@student.curtin.edu.au>
2025-07-17 13:51:12 +0800

    Added dpi to rcParams

--- src/psrutils/__init__.py
+++ src/psrutils/__init__.py
@@ -12,9 +12,12 @@ from .rm import *
 
 __version__ = version(__name__)
 
+rcParams["figure.dpi"] = 300
 rcParams["font.size"] = 12
 rcParams["mathtext.fontset"] = "dejavuserif"
 rcParams["font.family"] = "serif"
+
+# Uncomment only if LaTeX is installed
 # rcParams["text.usetex"] = True
 # rcParams["font.serif"] = "cm"
 
