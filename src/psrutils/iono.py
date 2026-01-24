6489d444702f5237d45a41a0836ed1ed9c9c883b
cplee1 <c.p.lee@student.curtin.edu.au>
2026-01-24 09:57:48 +0800

    Added log line for telescope location

--- src/psrutils/iono.py
+++ src/psrutils/iono.py
@@ -74,6 +74,7 @@ def get_rm_iono(
         observation.
     """
     if location.upper() in TELESCOPE_LOCS.keys():
+        logger.info(f"Using location: '{location.upper()}'")
         telescope_loc = TELESCOPE_LOCS[location.upper()]
     else:
         raise ValueError(f"Invalid location specified: '{location}'")
